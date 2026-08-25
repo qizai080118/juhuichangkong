6978d0ba815c813a84861de4b03395e6c1c89620
qizai080118 <douzidou123456@qq.com>
2026-08-25 11:47:20 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 function initPage(jsonPath, callback){
+    console.log("正在加载：",jsonPath);
     fetch(jsonPath)
-    .then(res=>res.json())
+    .then(res=>{
+        if(!res.ok){
+            throw new Error(`HTTP ${res.status} 文件找不到:${jsonPath}`);
+        }
+        return res.json();
+    })
     .then(data=>{
         callback(data);
     })
     .catch(err=>{
         console.error("加载json失败",err);
+        document.body.innerHTML += `<div style="color:red;padding:20px">数据加载异常：${err.message}</div>`;
     })
 }
